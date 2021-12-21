04d65cc34544917352674e97a42f0f69c1110260
KitzingDesign <jakob.kitzing@hotmail.com>
2021-12-21 19:28:36 +0100

    HOC probably fixed

--- components/HOC/withTransition.js
+++ components/HOC/withTransition.js
@@ -1,29 +1,34 @@
 import { motion } from "framer-motion";
 
-export default function WithTransition(OriginalComponent) {
-	return () => (
-		<div>
-			<OriginalComponent />
-			<motion.div
-				className="slide-in"
-				initial={{ scaleX: 0 }}
-				animate={{ scaleX: 0 }}
-				exit={{ scaleX: 1 }}
-				transition={{
-					duration: 1,
-					transition: "transform cubic-bezier(0.5, 0, 0.75, 0);",
-				}}
-			/>
-			<motion.div
-				className="slide-out"
-				initial={{ scaleX: 1 }}
-				animate={{ scaleX: 0 }}
-				exit={{ scaleX: 0 }}
-				transition={{
-					duration: 1,
-					transition: "transform  cubic-bezier(0.5, 0, 0.75, 0);",
-				}}
-			/>
-		</div>
-	);
-}
+const WT = (OriginalComponent) => {
+	const WithTransition = () => {
+		return (
+			<div>
+				<OriginalComponent />
+				<motion.div
+					className="slide-in"
+					initial={{ scaleX: 0 }}
+					animate={{ scaleX: 0 }}
+					exit={{ scaleX: 1 }}
+					transition={{
+						duration: 1,
+						transition: "transform cubic-bezier(0.5, 0, 0.75, 0);",
+					}}
+				/>
+				<motion.div
+					className="slide-out"
+					initial={{ scaleX: 1 }}
+					animate={{ scaleX: 0 }}
+					exit={{ scaleX: 0 }}
+					transition={{
+						duration: 1,
+						transition: "transform  cubic-bezier(0.5, 0, 0.75, 0);",
+					}}
+				/>
+			</div>
+		);
+	};
+	WithTransition.displayName = "WithTransition";
+	return WithTransition;
+};
+export default WT;
