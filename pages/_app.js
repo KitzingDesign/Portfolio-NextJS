2557bd0941f9728b19cd4423997b0e0b459b79ac
KitzingDesign <jakob.kitzing@hotmail.com>
2021-12-21 21:48:52 +0100

    view for phone v2

--- pages/_app.js
+++ pages/_app.js
@@ -12,10 +12,7 @@ function MyApp({ Component, pageProps, router }) {
 	return (
 		<Layout>
 			<Head>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
 			<AnimatePresence exitBeforeEnter>
 				<Component {...pageProps} key={router.route} />
