6cbd3f56c39e2780cb576e32709179524fe07560
KitzingDesign <jakob.kitzing@hotmail.com>
2022-01-02 18:19:42 +0100

    Ny motion - v2

--- components/HOC/withTransition.js
+++ components/HOC/withTransition.js
@@ -1,25 +1,34 @@
 import { motion } from "framer-motion";
 
-const withTransition = (OriginalComponent) => {
-	return () => (
-		<>
-			<OriginalComponent />
-			<motion.div
-				className="slide-in"
-				initial={{ scaleX: 0 }}
-				animate={{ scaleX: 0 }}
-				exit={{ scaleX: 1 }}
-				transition={{ duration: 1, ease: "easeInOut" }}
-			/>
-			<motion.div
-				className="slide-out"
-				initial={{ scaleX: 1 }}
-				animate={{ scaleX: 0 }}
-				exit={{ scaleX: 0 }}
-				transition={{ duration: 1, ease: "easeInOut" }}
-			/>
-		</>
-	);
+const WithTransition = (OriginalComponent) => {
+	const WT = () => {
+		return (
+			<div>
+				<OriginalComponent />
+				<motion.div
+					className="slide-in"
+					initial={{ scaleX: 0 }}
+					animate={{ scaleX: 0 }}
+					exit={{ scaleX: 1 }}
+					transition={{
+						duration: 1,
+						transition: "transform cubic-bezier(0.5, 0, 0.75, 0);",
+					}}
+				/>
+				<motion.div
+					className="slide-out"
+					initial={{ scaleX: 1 }}
+					animate={{ scaleX: 0 }}
+					exit={{ scaleX: 0 }}
+					transition={{
+						duration: 1,
+						transition: "transform  cubic-bezier(0.5, 0, 0.75, 0);",
+					}}
+				/>
+			</div>
+		);
+	};
+	WT.displayName = "WithTransition";
+	return WT;
 };
-
-export default withTransition;
+export default WithTransition;
