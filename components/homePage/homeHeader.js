52de49ca003e53405a0a635b3e428f382d3d5925
KitzingDesign <jakob.kitzing@hotmail.com>
2022-01-25 12:48:08 +0100

    trying emoations

--- components/homePage/homeHeader.js
+++ components/homePage/homeHeader.js
@@ -5,32 +5,64 @@ import styles from "./homeHeader.module.css";
 import Navigation from "../Layout/Navigation";
 import TextLoop from "../UI/textLoop";
 import BlobBackground from "../UI/blobBackground";
+import styled from "@emotion/styled";
 
 const text = "UX/UI.";
 
 const HomeHeader = () => {
+	const Container = styled.div`
+		height: 100vh;
+		width: 80vw;
+		margin: 0 auto;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	`;
+	const HeaderElement = styled.div`
+		display: flex;
+		height: 100%;
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+	`;
+
+	const HeaderText = styled.div`
+		width: 80%;
+		& > p {
+			line-height: 1.6;
+			width: 90%;
+			margin-bottom: 4.8rem;
+			margin-top: 3.2rem;
+		}
+		& > h1 {
+			font-size: 5.2rem;
+		}
+	`;
+
 	return (
-		<div className={styles.container}>
-			<div className={styles.headerElement}>
-				<div className={styles.headerText}>
-					<h1>Jakob Kitzing</h1>
+		<div>
+			<Container>
+				<HeaderElement>
+					<HeaderText>
+						<h1>Jakob Kitzing</h1>
 
-					<TextLoop />
+						<TextLoop />
 
-					<p>
-						Jag är en produktdesigner med huvudfokus på digitala produkter och
-						UX/UI. Min filosofi och det jag genuint brinner för är att utveckla
-						kreativa produkter och tjänster där användaren alltid står i
-						centrum.
-					</p>
-					<Link href="/about">
-						<a className={styles.btn}>Mer om mig</a>
-					</Link>
-				</div>
-				<div className={styles.headerImg}>
-					<BlobBackground />
-				</div>
-			</div>
+						<p>
+							Jag är en produktdesigner med huvudfokus på digitala produkter och
+							UX/UI. Min filosofi och det jag genuint brinner för är att
+							utveckla kreativa produkter och tjänster där användaren alltid
+							står i centrum.
+						</p>
+						<Link href="/about">
+							<a className={styles.btn}>Mer om mig</a>
+						</Link>
+					</HeaderText>
+					<div className={styles.headerImg}>
+						<BlobBackground />
+					</div>
+				</HeaderElement>
+			</Container>
 		</div>
 	);
 };
--- components/prosexPage/omProsex.js
+++ components/prosexPage/omProsex.js
@@ -4,17 +4,20 @@ import Image from "next/image";
 const OmProsex = () => {
 	return (
 		<div className={styles.container}>
-			<h2 data-aos="fade-up">Vad är prosex.?</h2>
-			<p data-aos="fade-up" className={styles.text}>
-				prosex. är teknisk designs sexmästeri och pr-förening. 2019-2020 satt
-				jag som pr-ansvarig i prosex. med det härliga gänget ni kan se nedan.
-				Mitt år som sittandes i prosex. var bland det roligaste jag gjort! Att
-				arbeta dag såväl som natt, att lösa problem i stressiga situationer och
-				att alltid försöka få ens arrangemang att bli så bra som möjligt kunde
-				ibland bli lite påfrestande, men att få se ett lyckat evenemang som vi
-				lyckats få ihop gjorde det värt alla dagar i veckan. Ett plus i det hela
-				är att jag även fick utveckla mina kunskaper i adobe suit under tiden.
-			</p>
+			<div className={styles.textContainer}>
+				<h2 data-aos="fade-up">Vad är prosex.?</h2>
+				<p data-aos="fade-up" className={styles.text}>
+					prosex. är teknisk designs sexmästeri och pr-förening. 2019-2020 satt
+					jag som pr-ansvarig i prosex. med det härliga gänget ni kan se nedan.
+					Mitt år som sittandes i prosex. var bland det roligaste jag gjort! Att
+					arbeta dag såväl som natt, att lösa problem i stressiga situationer
+					och att alltid försöka få ens arrangemang att bli så bra som möjligt
+					kunde ibland bli lite påfrestande, men att få se ett lyckat evenemang
+					som vi lyckats få ihop gjorde det värt alla dagar i veckan. Ett plus i
+					det hela är att jag även fick utveckla mina kunskaper i adobe suit
+					under tiden.
+				</p>
+			</div>
 			<div className={styles.imgContainer} data-aos="fade-up">
 				<Image
 					width="101%"
