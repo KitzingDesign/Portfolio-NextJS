151997a9498bd10a50e4d692a0b908af437f37dd
KitzingDesign <jakob.kitzing@hotmail.com>
2022-01-28 14:04:38 +0100

    Fixt up to digitaliserad

--- components/UI/app.js
+++ components/UI/app.js
@@ -1,14 +1,34 @@
 import styles from "./app.module.css";
 import Image from "next/image";
+import styled from "@emotion/styled";
 
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 2rem;
+	@media (max-width: 51em) {
+		gap: 2rem;
+	}
+`;
+const Text = styled.div`
+	color: white;
+	font-size: 4.8rem;
+	text-align: left;
+	margin-bottom: 1.2rem;
+	@media (max-width: 51em) {
+		font-size: 3.6rem;
+		margin-bottom: 0rem;
+	}
+`;
 const App = (props) => {
 	return (
-		<div className={styles.app}>
+		<Container>
 			<Image width={1000} height={1000} src={props.url} alt={props.alt} />
 			<div>
-				<h3>{props.header}</h3>
+				<Text>{props.header}</Text>
 			</div>
-		</div>
+		</Container>
 	);
 };
 
--- components/homePage/digitaliseringSection.js
+++ components/homePage/digitaliseringSection.js
@@ -4,6 +4,117 @@ import { ArrowRightIcon } from "@heroicons/react/solid";
 import Link from "next/link";
 
 import App from "../UI/app";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+	height: 100vh;
+	background-color: black;
+	height: inherit;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 10rem;
+	@media (max-width: 51em) and (orientation: landscape) {
+	}
+	@media (orientation: portrait) {
+		height: 100%;
+	}
+`;
+const Content = styled.div`
+	height: inherit;
+	align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 10rem;
+	& > h2 {
+		width: 80vw;
+		margin: 0 auto;
+		color: White;
+
+		font-size: 8rem;
+	}
+	@media (max-width: 51em) and (orientation: landscape) {
+		gap: 4rem;
+		& > h2 {
+			font-size: 6.4rem;
+		}
+	}
+	@media (orientation: portrait) {
+		gap: 6vh;
+		& > h2 {
+			font-size: 6.4rem;
+		}
+	}
+`;
+const Appar = styled.div`
+	width: 80vw;
+	margin: 0 auto;
+	display: grid;
+	grid-template-columns: repeat(5, 1fr);
+	gap: 2.4rem;
+
+	@media (max-width: 51em) and (orientation: landscape) {
+	}
+	@media (orientation: portrait) {
+		grid-template-columns: repeat(6, 1fr);
+
+		gap: 2.4rem;
+	}
+`;
+const Item = styled.div`
+	@media (orientation: portrait) {
+		grid-column: span 2;
+		&:nth-last-child(2) {
+			grid-column: 2 / span 2;
+		}
+	}
+`;
+const TheLink = styled.div`
+	&:hover {
+		font-style: italic;
+		cursor: pointer;
+	}
+	@media (orientation: portrait) {
+	}
+`;
+const ProjectLink = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	color: white;
+	box-sizing: inline-block;
+	color: white;
+
+	text-decoration: none;
+	cursor: pointer;
+`;
+
+const UtforskaText = styled.p`
+	font-size: 4.8rem;
+
+	@media (max-width: 51em) and (orientation: landscape) {
+		font-size: 3.6rem;
+	}
+	@media (orientation: portrait) {
+		font-size: 4.8rem;
+	}
+`;
+
+const Arrow = styled(ArrowRightIcon)`
+	color: white;
+
+	height: 9.6rem;
+	margin-left: 1rem;
+	@media (max-width: 51em) and (orientation: landscape) {
+		height: 6.4rem;
+	}
+	@media (orientation: portrait) {
+		height: 6.4rem;
+		margin-left: 1rem;
+	}
+`;
 
 let stay = false;
 const KrySection = (props) => {
@@ -11,63 +122,73 @@ const KrySection = (props) => {
 		stay = true;
 	}
 	return (
-		<div className={styles.container} id="Kry">
-			<div className={styles.content}>
+		<Container>
+			<Content>
 				<h2>
 					En Digitaliserad <br /> Värld
 				</h2>
-				<div className={styles.appar}>
-					<div
-						className={
-							stay ? `${styles.item} ${styles.animationOne}` : styles.item
-						}
-					>
-						<App url="/eldler_logga.svg" header="Eldler" alt="Eldler logo" />
-					</div>
-					<div
-						className={
-							stay ? `${styles.item} ${styles.animationTwo}` : styles.item
-						}
-					>
-						<App url="/ely_logga.svg" header="Ely" alt="Ely logo" />
-					</div>
-					<div
-						className={
-							stay ? `${styles.item} ${styles.animationThree}` : styles.item
-						}
-					>
-						<App url="/jury_logga.svg" header="Jury" alt="Jury logo" />
-					</div>
-					<div
-						className={
-							stay ? `${styles.item} ${styles.animationFour}` : styles.item
-						}
-					>
-						<App
-							url="/gyno_logga.svg"
-							header="Gyno"
-							id="icon_1"
-							alt="Gyno logo"
-						/>
-					</div>
-					<div
-						className={
-							stay ? `${styles.item} ${styles.animationFive}` : styles.item
-						}
-					>
-						<App url="/hemmy_logga.svg" header="Boi" alt="Boi logo" />
-					</div>
-				</div>
-				<div className={styles.link}>
-					<Link href="/digitalisering" passHref={true}>
-						<div className={styles.projectLink}>
-							<p>Utforska Projektet </p>
-							<ArrowRightIcon className={styles.icon} />
+				<Appar>
+					<Item>
+						<div
+							className={
+								stay ? `${styles.item} ${styles.animationOne}` : styles.item
+							}
+						>
+							<App url="/eldler_logga.svg" header="Eldler" alt="Eldler logo" />
 						</div>
+					</Item>
+					<Item>
+						<div
+							className={
+								stay ? `${styles.item} ${styles.animationTwo}` : styles.item
+							}
+						>
+							<App url="/ely_logga.svg" header="Ely" alt="Ely logo" />
+						</div>
+					</Item>
+					<Item>
+						<div
+							className={
+								stay ? `${styles.item} ${styles.animationThree}` : styles.item
+							}
+						>
+							<App url="/jury_logga.svg" header="Jury" alt="Jury logo" />
+						</div>
+					</Item>
+					<Item>
+						<div
+							className={
+								stay ? `${styles.item} ${styles.animationFour}` : styles.item
+							}
+						>
+							<App
+								url="/gyno_logga.svg"
+								header="Gyno"
+								id="icon_1"
+								alt="Gyno logo"
+							/>
+						</div>
+					</Item>
+					<Item>
+						<div
+							className={
+								stay ? `${styles.item} ${styles.animationFive}` : styles.item
+							}
+						>
+							<App url="/hemmy_logga.svg" header="Boi" alt="Boi logo" />
+						</div>
+					</Item>
+				</Appar>
+				<TheLink>
+					<Link href="/digitalisering" passHref={true}>
+						<ProjectLink>
+							<UtforskaText>Utforska Projektet</UtforskaText>
+							<Arrow />
+						</ProjectLink>
 					</Link>
-				</div>
-			</div>
-		</div>
+				</TheLink>
+			</Content>
+		</Container>
 	);
 };
 
