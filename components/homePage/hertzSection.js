adc5fca3fc589da1a69931055a5f66a3ee08e169
KitzingDesign <jakob.kitzing@hotmail.com>
2022-01-28 12:13:02 +0100

    Check if animation works

--- components/homePage/hertzSection.js
+++ components/homePage/hertzSection.js
@@ -2,43 +2,62 @@ import ProjectLink from "../UI/Project-link";
 import Image from "next/image";
 
 import styles from "./hertzSection.module.css";
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/react";
+import { useState } from "react";
+
 // import produktImg from "public/static/Macbook-mockup-hertz.jpg";
-const stay = false;
+// const TextAnimation = keyframes`
+// 	from {
+// 		opacity: 0;
+// 		transform: translate(0, 100vh);
+// 	}
+// 	to {
+// 		opacity: 1;
+// 		transform: translate(0, 0);
+// 	}`;
+
+const TextContainer = styled.div`
+	width: 40vw;
+`;
+const ImgContainer = styled.div`
+	width: 40vw;
+`;
+const Text = styled.p`
+	margin-top: 2.4rem;
+	margin-bottom: 2.4rem;
+`;
+
+const Texten = (props) => {
+	const [stay, setStay] = useState(true);
+};
 const HertzSection = (props) => {
-	if (props.fade) {
-		stay = true;
-	}
 	return (
 		<div className={styles.container} id="Hertz">
-			<div
-				className={
-					stay
-						? `${styles.contentText} ${styles.visableText}`
-						: styles.contentText
-				}
-			>
-				<h2>Biluthyrning</h2>
-				<p className={styles.text}>
-					Att känna frustration när man ska använda en hemsida är något som kan
-					skapa en negativ bild av hela varumärket. Jag och några kursare hade
-					som arbete att förbättra en biluthyrningshemsida ur ett usability
-					perspektiv, för att just slippa denna frustration.
-				</p>
-				<ProjectLink link="/Biluthyrning" />
+			<div className={`${styles.contentText} ${styles.visableText}`}>
+				<TextContainer>
+					<h2>Biluthyrning</h2>
+					<Text>
+						Att känna frustration när man ska använda en hemsida är något som
+						kan skapa en negativ bild av hela varumärket. Jag och några kursare
+						hade som arbete att förbättra en biluthyrningshemsida ur ett
+						usability perspektiv, för att just slippa denna frustration.
+					</Text>
+					<ProjectLink link="/Biluthyrning" />
+				</TextContainer>
 			</div>
-			<div
-				className={
-					stay ? `${styles.contentImg} ${styles.visableImg}` : styles.contentImg
-				}
-			>
-				<Image
-					width="100%"
-					height="100%"
-					layout="responsive"
-					objectFit="contain"
-					src="/Macbook-mockup-hertz.png"
-					alt="Mockup Car Rental webpage"
-				/>
+
+			<div className={`${styles.contentImg} ${styles.visableImg}`}>
+				<ImgContainer>
+					<Image
+						width="100%"
+						height="100%"
+						layout="responsive"
+						objectFit="contain"
+						src="/Macbook-mockup-hertz.png"
+						alt="Mockup Car Rental webpage"
+					/>
+				</ImgContainer>
 			</div>
 		</div>
 	);
